2b89889b3f932ef89da2042f747b3a141b112147
delightwook91 <delightwook@naver.com>
2018-08-03 19:51:46 +0900

    fixed vitrage.py

--- vitrage_dashboard/api/vitrage.py
+++ vitrage_dashboard/api/vitrage.py
@@ -108,70 +108,11 @@ def action_request(request, action, requestdict):
                  project_domain_id=project_domain_id)
 
     session = Session(auth=auth, timeout=600)
-    if str(action) == 'Mistral':
-        try:
-            from mistralclient.api import client as mistral_client
-        except ImportError:
-            mistral_client = None
-
-        if mistral_client is None:
-            LOG.warning('Failed to import mistralclient')
-        else:
-            mistralcli = mistral_client.client(session=session)
-            new_request = ast.literal_eval(requestdict)
-            actionname = new_request.keys()
-            workflow_input = new_request[actionname[0]]
-            mistralcli.executions.create(actionname[0],
-                                         workflow_input=workflow_input)
-
-    elif str(action) == 'Rally':
-
-        try:
-            from rally import api as rally_api
-        except ImportError:
-            rally_api = None
-
-        try:
-            from rally.cli.commands import task as rally_task
-        except ImportError:
-            rally_task = None
-
-        if rally_api is None or rally_task is None:
-            LOG.warning('Failed to import Rally')
-
-        else:
-            temp_dict = json.loads(requestdict)
-            key = temp_dict.keys()
-            if not temp_dict['syntaxcheck']:
-                key.remove('syntaxcheck')
-                file_path = './' + key[0] + '.json'
-                with open(file_path, 'w') as make_file:
-                    json.dump(temp_dict[key[0]], make_file)
-
-                rally_task.TaskCommands().start(rally_api.API(),
-                                                file_path,
-                                                tags=['Vitrage', key[0]])
-                os.remove(file_path)
-
-            else:
-                key.remove('syntaxcheck')
-                file_path = './' + key[0] + '.json'
-                with open(file_path, 'w') as make_file:
-                    json.dump(temp_dict[key[0]], make_file)
-                rally_task.TaskCommands().validate(rally_api.API(), file_path)
-
-                os.remove(file_path)
-
-
+    result = action_manager.ActionManager.execute(session, str(action),requestdict)
+    return result
 def action_setting(request):
     setting = ConfigParser.ConfigParser()
     setting.read('/etc/vitrage-dashboard/setting.conf')
-    actiondict = {'mistral': 'Mistral',
-                  'rally': 'Rally',
-                  'checkpoint': 'Checkpoint',
-                  'monitoring': 'Monitoring',
-                  'testing': 'Testing'
-                  }
     actionlist = []
     urllist = {}
     if setting.has_section('Default'):
@@ -180,35 +121,19 @@ def action_setting(request):
 
             for section in conf_actions:
                 result = None
-                if section != 'mistral' and section != 'rally' and section != 'checkpoint' and section != 'testing' and section != 'monitoring':
-                    if setting.has_section(section):
-                        option_list = setting.options(section)
-                        matching = [pro for pro in option_list
-                                    if ('url' in pro)]
-                        if matching:
-                            urllist[section] = setting.get(section,
+                if setting.has_section(section):
+                    option_list = setting.options(section)
+                    matching = [pro for pro in option_list
+                                if ('url' in pro)]
+                    if matching:
+                        urllist[section] = setting.get(section,
                                                            matching[0])
-
-                elif section == 'checkpoint':
-                    result = 'true'
-                elif section == 'monitoring':
-                    result = 'true'
-                elif section == 'testing':
-                    result = 'true'
-                elif section == 'mistral':
-                    result = base.is_service_enabled(request, 'workflowv2')
-                elif section == 'rally':
-                    try:
-                        rally_version = \
-                            importutils.try_import("rally.common.version")
-
-                        if rally_version is None:
-                            raise ImportError
-
-                        result = rally_version.version_string()
-                    except ImportError:
-                        LOG.warning('Failed to import Rally')
+                else:
+                    result = action_manager.ActionManager.importcheck(section,request)
 
                 if result:
-                    actionlist.append(actiondict[section])
+                    actionlist.append(section.capitalize())
         return [actionlist, urllist]
+
+
+
